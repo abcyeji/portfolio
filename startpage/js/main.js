0dfdf9dd003aadbd59cb6074a87dd141ad86e6c8
abcyeji <fndl0201@naver.com>
2025-07-24 18:06:59 +0900

    250724 코딩완료

--- startpage/js/main.js
+++ startpage/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
 			$('.cursor').css('left', e.pageX + 'px');
 			$('.cursor').css('top', e.pageY + 'px');
 		});
-		$('.popup').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+		$('section a').hover(function(){ /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
 			$('.cursor').toggleClass('on');
 		});
 
